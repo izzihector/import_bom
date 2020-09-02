5dc2f6ff2de383a78fe235ef2317f9c9c93f534f
Pavel Ushakov <p.v.ushakov@gmail.com>
2020-09-02 18:36:12 +0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Import bill of materials (BOM) from Excel or CSV File in Odoo',
+    'version': '13.0.0.0',
+    'sequence': 4,
+    'summary': 'Apps for Import multiple BOM import bom import bills of materials import componant on bom import mrp import manufacturing data from excel',
+    'category' : 'Manufacturing',
+    'description': """
+    BrowseInfo developed a new odoo/OpenERP module apps.
+    odoo import bill of materials add bill of materials import bills of materials
+    This module use for 
+    odoo import bulk bom from Excel file Import bill of material from CSV or Excel file import odoo
+    odoo Import bom lines Import multiple bom lines Import bill of material using excel Import BOM from csv
+    odoo BOM Import Add BOM from Excel odoo Add bom form csv Add CSV file Import bom data Import excel file
+    odoo bill of material import bom line import from excel import bill of material lines import
+   odoo import bill of materials  on odoo bill of materials import on odoo bills of material import on odoo
+odoo import mrp data import
+    odoo import manufacturing data in odoo import manufacturing data from csv dile
+    odoo import mrp data from csv file
+    """,
+    'author': 'BrowseInfo',
+    'website': 'http://www.browseinfo.in',
+    'depends': ['base', 'mrp'],
+    'data': [
+             "views/mrp.xml"
+             ],
+	'qweb': [
+		],
+    'demo': [],
+    "price": 15,
+    "currency": 'EUR',
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'live_test_url':'https://youtu.be/TEOK_epIrek',
+    'images':['static/description/Banner.png'],
+}
